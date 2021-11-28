d6ed19eed7462ae9aec8755adc0ab603d17b7694
taciossbr <tacioss.br@gmail.com>
2021-11-28 13:10:15 -0300

    fix: address auth

--- src/server.ts
+++ src/server.ts
@@ -21,10 +21,10 @@ server.put('/users/:id/credentials', loginRequired, credentialsController.update
 
 server.post('/login', credentialsController.login)
 
-server.post('/users/:userId/address/', addressesController.create)
-server.get('/users/:userId/address/', addressesController.list)
-server.put('/users/:userId/address/:id', addressesController.update)
-server.delete('/users/:userId/address/:id', addressesController.delete)
-server.get('/users/:userId/address/:id', addressesController.show)
+server.post('/users/:userId/address/', loginRequired, addressesController.create)
+server.get('/users/:userId/address/', loginRequired, addressesController.list)
+server.put('/users/:userId/address/:id', loginRequired, addressesController.update)
+server.delete('/users/:userId/address/:id', loginRequired, addressesController.delete)
+server.get('/users/:userId/address/:id', loginRequired, addressesController.show)
 
 export default server
